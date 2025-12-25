1061229cf9388aec7b6e3381e2970075af3ea732
Maxim Abramov <abramovmaxim.894@gmail.com>
2025-12-25 20:29:04 +0200

    remove prints

--- utils/rag.py
+++ utils/rag.py
@@ -64,9 +64,7 @@ class RAG:
 
     async def run(self, query: str) -> str:
         query_nodes_and_edges = await self._extract_nodes_and_edges_from_query(query)
-        print(query_nodes_and_edges)
         documents = self._graph_retrieve(query_nodes_and_edges)
-        print(len(documents))
         retriver = Neo4jVector.from_documents(
             documents,
             self.llm.embeddings,
